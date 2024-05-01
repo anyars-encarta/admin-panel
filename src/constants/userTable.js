e242cbd30f749c270cbf5dded556902e6b3914a3
anyars-encarta <anyarsencarta@gmail.com>
2024-05-01 13:48:56 +0000

    Restructure UserList component

--- src/constants/userTable.js
+++ src/constants/userTable.js
@@ -1,32 +1,3 @@
-import { DeleteOutline } from '@mui/icons-material';
-
-export const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    {
-        field: 'user', headerName: 'Username', width: 200, renderCell: (params) => {
-            return (
-                <div className='userListUser'>
-                    <img src={params.row.avatar} alt='' className='userListImage' />
-                    {params.row.username}
-                </div>
-            )
-        }
-    },
-    { field: 'email', headerName: 'Email', width: 200 },
-    { field: 'status', headerName: 'Status', width: 120 },
-    { field: 'transaction', headerName: 'Transaction Volume', width: 160 },
-    {
-        field: 'action', headerName: 'Action', width: 150, renderCell: (params) => {
-            return (
-                <>
-                    <button className="userListEdit">Edit</button>
-                    <DeleteOutline className="userListDelete" />
-                </>
-            )
-        }
-    },
-];
-
 export const rows = [
     { id: 1, username: 'Jon Snow', avatar: '/images/profile.jpg', email: 'jon@something.com', status: 'active', transaction: '$120.00', },
     { id: 2, username: 'Cersei Lannister', avatar: '/images/profile.jpg', email: 'cersei@something.com', status: 'inactive', transaction: '$420.00', },
--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-import { rows, columns } from '../../constants/userTable';
+import { rows } from '../../constants/userTable';
+import { DeleteOutline } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
 
 import './userList.css';
 
 const UserList = () => {
 
+    const [data, setData] = useState(rows);
+
+const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    {
+        field: 'user', headerName: 'Username', width: 200, renderCell: (params) => {
+            return (
+                <div className='userListUser'>
+                    <img src={params.row.avatar} alt='' className='userListImage' />
+                    {params.row.username}
+                </div>
+            )
+        }
+    },
+    { field: 'email', headerName: 'Email', width: 200 },
+    { field: 'status', headerName: 'Status', width: 120 },
+    { field: 'transaction', headerName: 'Transaction Volume', width: 160 },
+    {
+        field: 'action', headerName: 'Action', width: 150, renderCell: (params) => {
+            return (
+                <>
+                    <Link to={"/user/" + params.row.id} className='link'>
+                        <button className="userListEdit">Edit</button>
+                    </Link>
+
+                    <DeleteOutline className="userListDelete" />
+                </>
+            )
+        }
+    },
+];
+
     return (
         <div className='userList'>
             <DataGrid
-                rows={rows}
+                rows={data}
                 columns={columns}
                 disableRowSelectionOnClick
                 initialState={{
