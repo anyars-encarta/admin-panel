201f035e93310371673df081d461b0e9f215f035
anyars-encarta <anyarsencarta@gmail.com>
2024-05-01 12:15:20 +0000

    Add react-router and navigate to pages on click

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import './app.css';
 import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Topbar />
-      
+
       <div className='container'>
         <Sidebar />
-        <Home />
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/users' element={<UserList />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
