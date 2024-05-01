1ed7a8044545d341101baafe2b2e7070379cf3ef
anyars-encarta <anyarsencarta@gmail.com>
2024-05-01 13:32:04 +0000

    Separate table data in a separate file from component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './home.css';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
 import Chart from '../../components/chart/Chart';
-import { userData } from '../../constants/chat';
+import { userData } from '../../constants/user';
 import WidgetSmall from '../../components/widgetSmall/WidgetSmall';
 import WidgetBig from '../../components/widgetBig/WidgetBig';
 
