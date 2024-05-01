c891e28a8fae92d3772d873be5c506065063f5bc
anyars-encarta <anyarsencarta@gmail.com>
2024-05-01 18:22:32 +0000

    Create Products component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
+import Products from "./pages/products/Products";
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
           <Route path='/users' element={<UserList />} />
           <Route path='/user/:userId' element={<User />} />
           <Route path='/newUser' element={<NewUser />} />
+          <Route path='/products' element={<Products />} />
         </Routes>
       </div>
     </Router>
---
+++ src/pages/products/Products.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './products.css';
+
+const Products = () => {
+  return (
+    <div className='products'>Products</div>
+  )
+}
+
+export default Products
