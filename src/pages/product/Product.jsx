61f6ecc41360b05ef8ba02678fc79a4fff9b48cc
anyars-encarta <anyarsencarta@gmail.com>
2024-05-02 13:47:02 +0000

    Implement product edit page

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -3,6 +3,7 @@ import './product.css';
 import { Link } from 'react-router-dom';
 import Chart from '../../components/chart/Chart';
 import { productData } from '../../constants/chartData';
+import { Publish } from '@mui/icons-material';
 
 const Product = () => {
     const title = 'Sales Performance';
@@ -19,11 +20,11 @@ const Product = () => {
 
             <div className="productTop">
                 <div className="productTopLeft">
-                    <Chart title={title} data={productData} xDataKey='name' dataKey='Sales'/>
+                    <Chart title={title} data={productData} xDataKey='name' dataKey='Sales' />
                 </div>
                 <div className="productTopRight">
                     <div className="productInfoTop">
-                        <img src="/images/hp_laptop.jpg" alt="" className="productInfoImage" />
+                        <img src="/images/dell_desktop.jpg" alt="" className="productInfoImage" />
                         <span className="productName">HP Laptops</span>
                     </div>
                     <div className="productInfoBottom">
@@ -50,7 +51,36 @@ const Product = () => {
                 </div>
             </div>
 
-            <div className="productBottom"></div>
+            <div className="productBottom">
+                <form className="productForm">
+                    <div className="productFormLeft">
+                        <label for="pname">Product Name</label>
+                        <input id='pname' type="text" placeholder='HP Laptop' />
+
+                        <label for="inStock">In Stock</label>
+                        <select name="inStock" id="inStock">
+                            <option value="yes">Yes</option>
+                            <option value="no">No</option>
+                        </select>
+
+                        <label for="active">Active</label>
+                        <select name="active" id="active">
+                            <option value="yes">Yes</option>
+                            <option value="no">No</option>
+                        </select>
+                    </div>
+
+                    <div className="productFormRight">
+                        <div className="productUpload">
+                            <img src="/images/dell_desktop.jpg" alt="" className="productUploadImage" />
+                            <label for="file"><Publish className='productUpdateIcon' /></label>
+                            <input id='file' type="file" style={{ display: 'none' }} />
+                        </div>
+
+                        <button className="productButton">Update</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
