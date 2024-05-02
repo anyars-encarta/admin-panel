d28b94e90188cb5ed637ddbb48e0d23d08f591e1
anyars-encarta <anyarsencarta@gmail.com>
2024-05-02 14:19:28 +0000

    Implement add a new product

--- src/pages/newProduct/NewProduct.jsx
+++ src/pages/newProduct/NewProduct.jsx
@@ -2,9 +2,38 @@ import React from 'react';
 import './newProduct.css';
 
 const NewProduct = () => {
-  return (
-    <div className='newProduct'>NewProduct</div>
-  )
+    return (
+        <div className='newProduct'>
+            <h1 className="addProductTitle">New Product</h1>
+
+            <form className="addProductForm">
+                <div className="addProductItem">
+                    <label for="productImage">Image</label>
+                    <input id='productImage' type="file" />
+                </div>
+
+                <div className="addProductItem">
+                    <label for="productName">Product Name</label>
+                    <input id='productName' type="text" placeholder='product Name' />
+                </div>
+
+                <div className="addProductItem">
+                    <label for="productStock">Stock</label>
+                    <input id='productStock' type="text" placeholder='Stock' />
+                </div>
+
+                <div className="addProductItem">
+                    <label for="active">Active</label>
+                    <select name="active" id="active">
+                        <option value="yes">Yes</option>
+                        <option value="no">NO</option>
+                    </select>
+                </div>
+
+                <button className="addProductButton">Create</button>
+            </form>
+        </div>
+    )
 }
 
 export default NewProduct
